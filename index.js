d8a6c9535abcd42a1f65039ea84c7351d9ad0fbf
chao <dc10101@qq.com>
2017-06-07 22:14:13 +0800

    optimize memoryCache

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ exports.get = function (key) {
   }
   else{
     objString = wx.getStorageSync(key)
+    memoryCache[key] = objString
   }
   if(objString){
     const obj = JSON.parse(objString)
     if(obj.deadline && Date.now() > obj.deadline){
+      memoryCache[key] = undefined
+      wx.removeStorage({key: key})
       return undefined
     }
     else{
